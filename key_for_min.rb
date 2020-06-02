997421e52771f0b35b9856500b73766bec42f80b
interactive-directory-1839 <benthedunn@gmail.com>
2020-06-02 20:42:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  stored = 1000
+  stored = 1
   name_hash.each do |key, value|
     if value < stored
       stored = value
